33aa092f529920e230472b59e455d8fbf1017800
kim <eggkimkr@gmail.com>
2026-09-01 10:40:58 +0900

    배치 오류 수정

--- app/api/cron/sync-actual-lessons/route.js
+++ app/api/cron/sync-actual-lessons/route.js
@@ -1,6 +1,8 @@
 import handler from '../../../../server/handlers/cron/sync-actual-lessons.js';
 import { runApiHandler } from '@/lib/run-api-handler.js';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request) {
     return runApiHandler(handler, request);
 }
